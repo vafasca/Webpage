bb38fe499f48c605692157e93e6fce3a9cc82a0e
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2023-03-23 19:48:17 -0400

    added the news main modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule) }, { path: 'jobs', loadChildren: () => import('./features/jobs/jobs.module').then(m => m.JobsModule) }, { path: 'contact', loadChildren: () => import('./features/contact/contact.module').then(m => m.ContactModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
