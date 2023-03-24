5b07fe8dfe36e9ada0ff1863226dd14bcc914d6d
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2023-03-23 20:03:46 -0400

    added default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule) }, { path: 'jobs', loadChildren: () => import('./features/jobs/jobs.module').then(m => m.JobsModule) }, { path: 'contact', loadChildren: () => import('./features/contact/contact.module').then(m => m.ContactModule) }];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./features/about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'jobs',
+    loadChildren: () =>
+      import('./features/jobs/jobs.module').then((m) => m.JobsModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./features/contact/contact.module').then((m) => m.ContactModule),
+  },
+  {
+    path: '**', redirectTo: '/home', pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
