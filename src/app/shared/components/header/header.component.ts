b3b1fc857936049dcd0141e454997368244815b8
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2023-03-30 16:02:49 -0400

    added local logo image, new route

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,30 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  responsiveNav!: HTMLElement | null;
-  nav!: HTMLElement | null;
-  test!: boolean;
-  showNav = true;
-  showNav2 = false;
-  about = '/about';
+  logo:string = "../../../../assets/images/Group-169413.png";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  toggleResponsiveNav() {
-    this.responsiveNav = document.getElementById('responsive-nav');
-    this.nav = document.getElementById('prueba');
-    console.log(this.responsiveNav);
-    console.log(this.nav);
-    if (this.responsiveNav !== null && this.nav !== null) {
-    this.responsiveNav.style.display = 'inline-block';
-    }
-  }
-  // toggleResponsiveNav(){
-  //   console.log(this.showNav);
-  //   this.showNav = !this.showNav;
-  //   this.showNav2 = !this.showNav;
-  // }
+
+
 }
