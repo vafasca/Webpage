62f03bf79c3ab112bc10ef1dbbf7ca825a9186a1
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2024-08-17 16:39:52 -0400

    revert last comments

--- src/app/features/home/home-testimonial/home-testimonial.component.ts
+++ src/app/features/home/home-testimonial/home-testimonial.component.ts
@@ -8,90 +8,67 @@
   })
   export class HomeTestimonialComponent implements OnInit {
 
-    // customOptions: any = {
-    //   loop: true,
-    //   autoplay: true,
-    //   autoplayTimeout: 5000, // Cambia automáticamente cada 3 segundos
-    //   autoplayHoverPause: true,
-    //   items: 3, // Muestra 3 cards a la vez
-    //   margin: 10, // Espacio entre las cards
-    //   nav: false, // Oculta las flechas de navegación
-    //   responsive: {
-    //     0: {
-    //       items: 1 // Muestra 1 card en pantallas pequeñas
-    //     },
-    //     600: {
-    //       items: 2 // Muestra 2 cards en pantallas medianas
-    //     },
-    //     1000: {
-    //       items: 3 // Muestra 3 cards en pantallas grandes
-    //     }
-    //   }
-    // };
+ customOptions: any = {
+       loop: true,
+       autoplay: true,
+       autoplayTimeout: 5000, // Cambia automáticamente cada 3 segundos
+       autoplayHoverPause: true,
+       items: 3, // Muestra 3 cards a la vez
+       margin: 10, // Espacio entre las cards
+       nav: false, // Oculta las flechas de navegación
+       responsive: {
+         0: {
+           items: 1 // Muestra 1 card en pantallas pequeñas
+         },
+         600: {
+           items: 2 // Muestra 2 cards en pantallas medianas
+         },
+         1000: {
+           items: 3 // Muestra 3 cards en pantallas grandes
+         }
+       }
+     };
 
-    // slidesStore = [
-    //   {
-    //     id: 1,
-    //     src: 'https://digitalharborbolivia.com/wp-content/uploads/2023/05/Rommel.jpg',
-    //     alt: 'Rommel Loayza',
-    //     title: 'Ser un DHR',
-    //     comment: 'Somos apasionados y estamos locos. Eso nos hace WOW y por eso vamos a cambiar el mundo.',
-    //     name: 'Rommel Loayza',
-    //     position: 'Software Architect'
-    //   },
-    //   {
-    //     id: 2,
-    //     src: 'https://digitalharborbolivia.com/wp-content/uploads/2023/05/Scott.jpg',
-    //     alt: 'Scott Wood',
-    //     title: 'Digital Harbor',
-    //     comment: 'Digital Harbor is transforming how we work in unexpected and powerful ways',
-    //     name: 'Scott Wood',
-    //     position: 'Director of Engineering'
-    //   },
-    //   {
-    //     id: 3,
-    //     src: 'https://digitalharborbolivia.com/wp-content/uploads/2023/05/Mike.jpg',
-    //     alt: 'Mike Tingey',
-    //     title: 'Innovation at DH',
-    //     comment: 'Innovation at DH is an idea that takes a leap beyond conventional thinking, which greatly improves the usability of a computer',
-    //     name: 'Mike Tingey',
-    //     position: 'Chief Architect'
-    //   },
-    //   {
-    //     id: 4,
-    //     src: 'https://digitalharborbolivia.com/wp-content/uploads/2023/05/Rohit.jpg',
-    //     alt: 'Rohit Agarwal',
-    //     title: 'Changing the World',
-    //     comment: 'It requires to be curious to change the world, and to do it, it requires to have courage...',
-    //     name: 'Rohit Agarwal',
-    //     position: 'President and Chief Innovations Officer'
-    //   }
-    // ];
+     slidesStore = [
+       {
+         id: 1,
+         src: 'https://digitalharborbolivia.com/wp-content/uploads/2023/05/Rommel.jpg',
+         alt: 'Rommel Loayza',
+         title: 'Ser un DHR',
+         comment: 'Somos apasionados y estamos locos. Eso nos hace WOW y por eso vamos a cambiar el mundo.',
+         name: 'Rommel Loayza',
+         position: 'Software Architect'
+       },
+       {
+         id: 2,
+         src: 'https://digitalharborbolivia.com/wp-content/uploads/2023/05/Scott.jpg',
+         alt: 'Scott Wood',
+         title: 'Digital Harbor',
+         comment: 'Digital Harbor is transforming how we work in unexpected and powerful ways',
+         name: 'Scott Wood',
+         position: 'Director of Engineering'
+       },
+       {
+         id: 3,
+         src: 'https://digitalharborbolivia.com/wp-content/uploads/2023/05/Mike.jpg',
+         alt: 'Mike Tingey',
+         title: 'Innovation at DH',
+         comment: 'Innovation at DH is an idea that takes a leap beyond conventional thinking, which greatly improves the usability of a computer',
+         name: 'Mike Tingey',
+         position: 'Chief Architect'
+       },
+       {
+         id: 4,
+         src: 'https://digitalharborbolivia.com/wp-content/uploads/2023/05/Rohit.jpg',
+         alt: 'Rohit Agarwal',
+         title: 'Changing the World',
+         comment: 'It requires to be curious to change the world, and to do it, it requires to have courage...',
+         name: 'Rohit Agarwal',
+         position: 'President and Chief Innovations Officer'
+       }
+     ];
 
-    customOptions: OwlOptions = {
-      loop: true,
-      mouseDrag: true,
-      touchDrag: true,
-      pullDrag: true,
-      dots: true,
-      navSpeed: 700,
-      navText: ['', ''],
-      responsive: {
-        0: {
-          items: 1
-        },
-        400: {
-          items: 2
-        },
-        740: {
-          items: 3
-        },
-        940: {
-          items: 4
-        }
-      },
-      nav: true
-    }
+
 
     constructor() { }
 
