3742e22bba6a876ed726ac5c13efcdca93bfe0da
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2024-08-18 02:35:37 -0400

    added BrowserAnimationsModule

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -33,7 +34,8 @@ import { HomeTestimonialComponent } from './home-testimonial/home-testimonial.co
     CommonModule,
     HomeRoutingModule,
     SharedModule,
-    CarouselModule
+    CarouselModule,
+    BrowserAnimationsModule
   ]
 })
 export class HomeModule { }
