503713c460b6723a4c9836441a5b343fa32ff06d
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2024-08-25 19:45:39 -0400

    added Angular PWA support with service worker and manifest configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
 import { AngularMaterialModule } from './shared/modules/angular-material/angular-material.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 // register Swiper custom elements
 register();
 
@@ -19,7 +21,13 @@ register();
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
