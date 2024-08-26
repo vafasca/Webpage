645a8fe327e289ead58d06bc5c10ed4f9ed6b88d
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2024-08-26 14:29:28 -0400

    Added styles to the footer and an arrow to show the top of the page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isButtonVisible: boolean = false;
 
-  hola: string = 'https://dl.dropboxusercontent.com/s/b4ej0tf2pe2ycz3/a.png'
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isButtonVisible = window.pageYOffset > 300;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
   constructor() { }
 
